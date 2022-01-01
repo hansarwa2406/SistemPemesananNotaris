c3a2479aad3a4c052f6811bde97e1b976dbf303c
hansarwa2406 <kuronokameraman@gmail.com>
2022-01-02 01:06:58 +0700

    Update Client.php

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+    public function getRouteKeyName()
+    {
+        return 'id_klien';
+    }
+
+    public function booking()
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
