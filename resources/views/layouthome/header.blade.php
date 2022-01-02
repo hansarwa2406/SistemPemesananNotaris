1dce25845a15c3c2d2a7a459c06f3e2481a1bb95
hansarwa2406 <kuronokameraman@gmail.com>
2022-01-02 18:20:27 +0700

    Update navbar all

--- resources/views/layouthome/header.blade.php
+++ resources/views/layouthome/header.blade.php
@@ -9,7 +9,7 @@
             <ul class="nav__list">
                 <li class="nav__item">
                     {{-- <a href="#home" class="nav__link active-link">Home</a> --}}
-                    <a href="/dashboard/klien" class="nav__link active-link">Home</a>
+                    <a href="#home" class="nav__link active-link">Home</a>
                 </li>
                 <li class="nav__item">
                     <a href="#about" class="nav__link">Tentang Kami</a>
@@ -23,6 +23,32 @@
                 <li class="nav__item">
                     <a href="#contact" class="nav__link">Kontak Kami</a>
                 </li>
+
+                @auth
+                    <li class="nav__item">
+                        <a href="/dashboard" class="nav__link"><i class="bi bi-box-arrow-in-right"></i> Akun</a>
+                    </li>
+
+                    {{-- <li class="nav__item dropdown">
+                        <a class="nav__link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Welcome back, {{ auth()->user()->name }}
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-window-reverse"></i> Dashboard</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li>
+                                <form action="/logout" method="POST">
+                                    @csrf
+                                    <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout</button>
+                                </form>
+                            </li>
+                        </ul>
+                    </li> --}}
+                @else
+                    <li class="nav__item">
+                        <a href="/login" class="nav__link"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+                    </li>
+                @endauth
             </ul>
 
             <div class="nav__close" id="nav-close">
