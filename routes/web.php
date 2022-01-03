80698045ca3ee89308ff0da89a2c6d299792ca84
hansarwa2406 <kuronokameraman@gmail.com>
2022-01-03 16:44:28 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -73,6 +73,3 @@ Route::get('/dashboard', function(){
 
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
-
-// Admin Category Controller
-Route::resource('/dashboard/categories', AdminCategoryController::class);
